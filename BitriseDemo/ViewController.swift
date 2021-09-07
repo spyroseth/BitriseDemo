6fb975eb045c53097b44d3ec7b33ed6f474a66de
Spyros Spatharis <s.spatharis@skroutz.gr>
2021-09-07 16:25:30 +0300

    Initial Commit

---
+++ BitriseDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BitriseDemo
+//
+//  Created by Spyros Spatharis on 7/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
